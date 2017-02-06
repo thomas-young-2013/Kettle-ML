985e93e190590aefa86403ec4d985db81f96c349
thomas-young-2013 <1225646303@qq.com>
2017-02-06 17:26:02 +0800

    add linear regressor

--- engine/org/pentaho/di/trans/steps/linearregression/LinearRegressor.java
+++ engine/org/pentaho/di/trans/steps/linearregression/LinearRegressor.java
@@ -1,7 +1,42 @@
-package org.pentaho.di.trans.steps.linearregression;
-
 /**
  * Created by thomasyngli on 2017/2/5.
  */
-public class LinearRegressor {
+
+package org.pentaho.di.trans.steps.linearregression;
+
+import org.apache.commons.vfs2.FileObject;
+import org.apache.commons.vfs2.FileSystemException;
+import org.pentaho.di.core.Const;
+import org.pentaho.di.core.exception.KettleException;
+import org.pentaho.di.core.exception.KettleFileException;
+import org.pentaho.di.core.exception.KettleValueException;
+import org.pentaho.di.core.row.RowMetaInterface;
+import org.pentaho.di.core.row.ValueMetaInterface;
+import org.pentaho.di.core.vfs.KettleVFS;
+import org.pentaho.di.i18n.BaseMessages;
+import org.pentaho.di.trans.Trans;
+import org.pentaho.di.trans.TransMeta;
+import org.pentaho.di.trans.step.BaseStep;
+import org.pentaho.di.trans.step.StepDataInterface;
+import org.pentaho.di.trans.step.StepInterface;
+import org.pentaho.di.trans.step.StepMeta;
+import org.pentaho.di.trans.step.StepMetaInterface;
+
+import java.util.List;
+
+public class LinearRegressor extends BaseStep implements StepInterface {
+    private static Class<?> PKG = LinearRegressor.class; // for i18n
+
+    public LinearRegressor( StepMeta stepMeta, StepDataInterface stepDataInterface,
+                     int copyNr, TransMeta transMeta, Trans trans ) {
+        super( stepMeta, stepDataInterface, copyNr, transMeta, trans );
+    }
+
+    @Override
+    public boolean processRow( StepMetaInterface smi, StepDataInterface sdi ) throws KettleException {
+        // wait for first for is available
+        Object[] r = getRow();
+        List<String> groupFields = null;
+        return false;
+    }
 }
--- engine/org/pentaho/di/trans/steps/linearregression/LinearRegressorData.java
+++ engine/org/pentaho/di/trans/steps/linearregression/LinearRegressorData.java
@@ -1,7 +1,70 @@
-package org.pentaho.di.trans.steps.linearregression;
-
 /**
  * Created by thomasyngli on 2017/2/5.
  */
-public class LinearRegressorData {
+
+package org.pentaho.di.trans.steps.linearregression;
+
+import org.apache.commons.vfs2.FileObject;
+import org.pentaho.di.core.row.RowMetaInterface;
+import org.pentaho.di.trans.step.BaseStepData;
+import org.pentaho.di.trans.step.StepDataInterface;
+import org.pentaho.di.trans.steps.sort.RowTempFile;
+
+import java.io.DataInputStream;
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+import java.util.zip.GZIPInputStream;
+
+public class LinearRegressorData extends BaseStepData implements StepDataInterface {
+    public List<FileObject> files;
+    public List<Object[]> buffer;
+    public int getBufferIndex;
+
+    public List<InputStream> fis;
+    public List<GZIPInputStream> gzis;
+    public List<DataInputStream> dis;
+    public List<Object[]> rowbuffer;
+    public List<Integer> bufferSizes;
+
+    // To store rows and file references
+    public List<RowTempFile> tempRows;
+
+    public int[] fieldnrs; // the corresponding field numbers;
+    public FileObject fil;
+    public RowMetaInterface outputRowMeta;
+    public int sortSize;
+    public boolean compressFiles;
+    public int[] convertKeysToNative;
+    public boolean convertAnyKeysToNative;
+
+    Comparator<RowTempFile> comparator;
+    Comparator<Object[]> rowComparator;
+
+    public int freeCounter;
+    public int freeMemoryPct;
+    public int minSortSize;
+    public int freeMemoryPctLimit;
+    public int memoryReporting;
+
+    /*
+     * Group Fields Implementation heroic
+     */
+    public Object[] previous;
+    public int[] groupnrs;
+    public boolean newBatch;
+
+    public LinearRegressorData() {
+        super();
+
+        files = new ArrayList<FileObject>();
+        fis = new ArrayList<InputStream>();
+        gzis = new ArrayList<GZIPInputStream>();
+        dis = new ArrayList<DataInputStream>();
+        bufferSizes = new ArrayList<Integer>();
+
+        previous = null; // Heroic
+    }
+
 }
